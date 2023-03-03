66a85f88a729784ec332d68798851a0d7b6ade3e
Chris Martin <crm28@psu.edu>
2023-03-03 12:45:52 -0500

    Proofed hist function of wire.py

--- wire.py
+++ wire.py
@@ -1,257 +1,34 @@
 #!/usr/bin/python3
 """Spinning Disc Langmuir Probe (SDLP) spatial measurement utilities
 
-wire.py <command> <infile> <outfile>
+# Print help and exit
+    $ wire.py -h
+    # wire.py -h <command>
+
+# Collect statistics on the wire file
+    $ wire.py [-c config] stat <wirefile> <target>
+    
+# Construct a view of the output of wsolve
+    # wire.py [-c config] view <wsolvefile> <target>
 """
 
 import numpy as np
-from scipy.linalg import solve
 import array,struct
-import time
 import matplotlib.pyplot as plt
-import getopt
+from matplotlib.patches import Polygon
+import sys
+from getopt import getopt
 
 
 
-class WireSlice(object):
-    """WireSlice - ion densities in a 2D slice constructed from SDLP measurements
+help_text = {
+    'hist':"""$ wire.py [-c config] hist <wirefile> <target>
 
-    ws = WireSlice(L, ...)
-    
-** L **
-The domain size can be specified as a tuple for a rectuangular domain
-    ws = WireSlice(L = (width, height), ...)
-or with a scalar for a square domain
-    ws = WireSlice(L = side, ...)
-
-No length units are presumed, but all lengths used by WireSlice 
-instances must be the same.  For display purposes, an optional unit 
-string may be specified using the length keyword.  
-    ws = WireSlice( ... units='mm', ...)
-    
-** Defining the Expansion **
-The number of terms used in the Fourier expansion in x (horizontal) and
-y (vertical) can be specified explicitly by N or implicitly by the 
-highest wavenumber, k.  Either N or k is required.
-
-** N **
-The number of non-zero wavenumbers in each axis can be independently 
-specified by a tuple,
-    ws = WireSlice( ... N = (Nx, Ny), ...)
-or with a scalar to specify them to be the same 
-    ws = WireSlice( ... N = number, ...)
-
-The total number of terms in the expansion is
-    (Nx + 1) * (Ny + 1).
-One is added to each dimension to include the constant term.
-
-** k **
-The maximum wavenumber is an alternative means for determining the number
-of terms in the expansion.  A wavenumber is the spatial equivalent to a
-frequency.  With units 1/length, it specifies the number of wave periods
-in a unit length.  Therefore, the maximum wavenumber along an axis is 
-k=N/L.
-
-The maximum wavenumber can be specified for each axis in a tuple
-    ws = WireSlice( ... k = (kx, ky), ...)
-or with a scalar to specify them to be the same
-    ws = WireSlice( ... k = number, ...)
-    
-The maximum wavenumber determines the tightest spatial gradient that can
-be represented in the expansion.  The inverse of the maximum wavenumber
-is a measure of the spatial resolution that can be represented by the
-expansion.
+Collect statistics on the data in the wirefile using wsolve's configuration
+file.  
 """
-    
-    def __init__(self, L, N=None, k=None, units=''):
-        # Force L to be a two-element vector
-        
-        self.L = np.broadcast_to(np.asarray(L, dtype=int), (2,))
-        self.N = np.zeros([2], dtype=int)   # number of wavenumbers
-        self.units = str(units)
-        self.points = 0
 
-
-        if N is not None:
-            if k is not None:
-                raise Exception('Cannot specify both N and k simultaneously')
-            self.N[:] = N
-        elif k is not None:
-            self.N[:] = np.floor(k/self.L)
-        else:
-            raise Exception('Either N or k must be specified.')
-           
-        self.ncoef = np.prod(2*self.N+1)
-
-        # establish wavenumber vectors
-        self.nu = np.meshgrid(
-                np.arange(-self.N[0], self.N[0]+1)/self.L[0],
-                np.arange(-self.N[1], self.N[1]+1)/self.L[1])
-        self.nu[0] = self.nu[0].reshape((self.ncoef,))
-        self.nu[1] = self.nu[1].reshape((self.ncoef,))
-        
-        # initialize a solution matrix and vectors
-        self.C = None
-        self.I0 = None
-        self.A = np.zeros((self.ncoef, self.ncoef), dtype=complex)
-        self.B = np.zeros((self.ncoef,), dtype=complex)
-        
-        
-    def __call__(self, x, y):
-        x,y = np.broadcast_arrays(x,y)
-        zshape = x.shape
-        x,y = x.reshape((x.size,1)),y.reshape((y.size,1));
-        
-        nux = self.nu[0].reshape((1,self.ncoef))
-        nuy = self.nu[1].reshape((1,self.ncoef))
-        
-        z = np.dot( np.exp(2j*np.pi*(nux*x + nuy*y)), self.C).real
-        
-        return z.reshape(zshape)
-        
-        
-    def lam(self, r, d, theta):
-        """LAM - calculate the lambda vector for a given d and theta
-"""
-        # Does this theta actually intersect the domain?
-        theta_critical = np.arctan(self.L[1] / (2*d))
-        if not -theta_critical < theta < theta_critical or r<=d:
-            return np.zeros((self.ncoef,),dtype=complex)
-            #LAM[self.flatindex(0,0)] = 1.0j
-            #return LAM
-   
-        # Sines and cosines will come in handy repeatedly
-        sth = np.sin(theta)
-        cth = np.cos(theta)
-        
-        # Initialize the gamma values
-        gam = np.empty((self.ncoef,), dtype=complex)
-        
-        # Calculate R1
-        # Since these raidii calculations can be infinite, first calculate
-        # the maximum inverse of radius, which will never be zero.
-        # Then, we'll invert it again.
-        r1 = 1./max(1./r, 
-                cth/(self.L[0] + d), 
-                np.abs(2*sth/self.L[1]))
-                
-        # Calculate R0
-        r0 = d/cth
-
-        # Calculate a wavenumber matrix
-        nu = self.nu[0]*cth + self.nu[1]*sth
-        # Izero is a boolean index marking elements where k is zero
-        Izero = (nu == 0)
-        # Force nu to a non-zero number to avoid division by zero
-        # We will overwrite these values later
-        gam[Izero] = r1-r0
-        Izero = np.logical_not(Izero)
-        # Calculate the line integral matrix
-        gam[Izero] = (np.exp(2j*np.pi*nu[Izero]*r1) - np.exp(2j*np.pi*nu[Izero]*r0)) / (2j*np.pi*nu[Izero])
-
-        # gam is now the "gamma" value described in the paper.  We will
-        # now, further modify the variable by the d-exponential 
-        return np.exp(-2j*np.pi*self.nu[0]*d) * gam
-
-
-    def flatindex(self, m, n):
-        return np.ravel_multi_index(self.N+[m,n], 2*self.N+1)
-
-    def mnindex(self, index):
-        return np.unravel_index(index, 2*self.N+1)-self.N
-
-    def include(self, r, d, theta, I):
-        """INCLUDE - include a datum in the model
-    include(r, d, theta, I)
-    
-d   -   distance between the center of rotation and the domain edge
-theta - the wire angle in radians
-I   -   The total current measured in this position
-"""
-        lam = self.lam(r, d, theta)
-        self.A += lam.reshape((lam.size,1)) * lam
-        self.B += lam*I
-        self.points += 1
-
-    def read(self, filename):
-        """Read from a file
-    ws.read(filename)
-        OR
-    ws.read(wf)
-    
-Reads from a file or from an open WireFile instance
-"""
-        if isinstance(filename,str):
-            wf = WireFile(filename)
-            with wf.open('r'):
-                self.read(wf)
-            return
-        elif not isinstance(filename,WireFile):
-            raise Exception('The file must be either a string path or a WireFile instance.')
-
-        
-        def worker(ws, wf, A, B, n, ll):
-            for r,d,theta,I in wf:
-                ws.include(r,d,theta,I)
-            ll.acquire()
-            try:
-                A += ws.A.flatten()
-                B += ws.B
-                n += ws.points
-            finally:
-                ll.release()
-                
-        lock = mp.Lock()
-        A = mp.Array('d', 2*int(self.ncoef*self.ncoef))
-        B = mp.Array('d', 2*int(self.ncoef))
-        n = mp.Value(int)
-        workers = []
-        for index in range(mp.cpu_count()):
-            w = mp.Process(target=worker, args=(self, filename, A, B, n, lock))
-            w.start()
-            workers.append(w)
-        for w in workers:
-            w.join()
-            
-        print(A)
-
-
-
-    def solve(self):
-        """SOLVE - solve the system with the data already included
-"""
-        #self.C = np.linalg.solve(self.A, self.B)
-        self.C = solve(self.A, self.B, assume_a = 'sym')
-        
-
-    def grid(self,Nx=None, Ny=None):
-        """GRID - evaluate the solution at grid points
-    x,y,I = grid()
-        OR
-    x,y,I = grid(N)
-        Or
-    x,y,I = grid(Nx, Ny)
-    
-If no argument is given, the grid spacing is selected automatically 
-based on the highest wave number in each axis.  If a single scalar 
-argument is given, it is treated as the number of grid oints in each
-axis.  If tuple pair of arguments are found, they are interpreted as 
-the number of grid points in the x- and y-axes.
-"""
-        # If no arguments are given
-        if Nx is None:
-            if Ny is None:
-                Nx,Ny = 2*self.N+1
-            else:
-                raise Exception('Cannot specify Ny without specifying Nx.')
-        # If Nx is given
-        elif Ny is None:
-            Ny = Nx
-        # If Nx and Ny are given, do nothing        
-        x = np.linspace(0,self.L[0],Nx)
-        y = np.linspace(-self.L[1]/2, self.L[1]/2, Ny)
-        x,y = np.meshgrid(x,y)
-        return x,y
+}
 
 
 class WireFile:
@@ -335,7 +112,26 @@ Returns an empty tuple if end-of-file
             raise Exception('The file is not opened in write mode.')
         self.fd.write(struct.pack(self.lineformat, r,d,theta,I))
         
-
+    def read(self):
+        """Read all data from the file
+    r,d,theta,I = wf.read()
+    
+Reads in numpy arrays of radius, distance, angle, and current.
+"""
+        if not self.isread or self.fd is None:
+            raise Exception('The file is not opened in read mode.')
+        R = []
+        D = []
+        T = []
+        I = []
+        for r,d,t,i in self:
+            R.append(r)
+            D.append(d)
+            T.append(t)
+            I.append(i)
+        return np.array(R), np.array(D), np.array(T), np.array(I)
+            
+        
 
 class SliceData:
     def __init__(self, filename):
@@ -458,23 +254,109 @@ the number of grid points in the x- and y-axes.
         plt.show()
 
 if __name__ == '__main__':
-    opts,args = getopt.getopt(sys.argv[1:], 'c:')
+    opts,args = getopt(sys.argv[1:], 'c:h')
+    configfile = 'wsolve.conf'
+    config = {}
     
-    if args[0].lower() == 'stat':
-        infile = args[1]
-        R = []
-        D = []
-        T = []
-        I = []
-        with WireFile(infile).open('r') as wf:
-            for r,d,theta,i in wf:
-                R.append(r)
-                D.append(d)
-                T.append(theta)
-                I.append(i)
-                
-        
+    # Scan the options
+    for oo  in opts:
+        if oo[0] == '-h':
+            if len(args) == 0:
+                print(__doc__)
+                exit(0)
+            elif args[0] in help_text:
+                print(help_text[args[0]])
+                exit(0) 
+            else:
+                print("Unrecognized command: " + args[0])
+                print(__doc__)
+                exit(0)
+        elif oo[0] == '-c':
+            configfile = oo[1]
+    ##
+    ## Read in the configuration file
+    ##
+    params = [('nthread', int), 
+            ('Nx', int), ('Ny', int), 
+            ('Lx', float), ('Ly', float),
+            ('dshift', float)]
             
+    with open(configfile,'r') as fd:
+        words = fd.read().split()
+    
+    for pstr, ptype in params:
+        pfound = words.pop(0)
+        if pfound != pstr:
+            raise Exception('Configuration syntax error in ' + configfile + '. Expected: ' + pstr + ' Found: ' + pfound)
+        try:
+            config[pstr] = ptype(words.pop(0))
+            print(f'{pstr:>16s} : {config[pstr]:<}')
+        except:
+            raise Exception('Configuration syntax error in ' + configfile + '. Failed while parsing: ' + pstr)
+            
+    if args[0].lower() == 'hist':
+        if len(args)!=3:
+            print(help_text['hist'])
+            raise Exception('After command "hist" two arguments are expected.')
+            
+        infile = args[1]
+        target = args[2]
+        # Read in the data
+        with WireFile(infile).open('r') as wf:
+            r,d,theta,I = wf.read()
+        # Apply the configured shift
+        d += config['dshift']
+        # Calculate x,y coordinates of the wire tip
+        x = r*np.cos(theta) - d
+        y = r*np.sin(theta)
+        # Calculate the histogram bin sizes
+        xbin = config['Lx']/(2*config['Nx'])
+        ybin = config['Ly']/(2*config['Ny'])
+        # Calculate coordinate bin indices
+        xi = np.array(np.floor(x / xbin), dtype=int)
+        yi = np.array(np.floor(y / ybin), dtype=int)
+        # Find the maximum and minimum limits
+        xi_min = np.min(xi)
+        xi_max = np.max(xi)
+        nx = xi_max - xi_min + 1
+        
+        yi_min = np.min(yi)
+        yi_max = np.max(yi)
+        ny = yi_max - yi_min + 1
+        
+        count = np.zeros((ny,nx), dtype=int)
+        
+        for xii,yii in zip(xi,yi):
+            count[yii-yi_min,xii-xi_min] += 1
+            
+        
+        xx = np.arange(xi_min,xi_max+2)*xbin
+        yy = np.arange(yi_min,yi_max+2)*ybin
+        xx,yy = np.meshgrid(xx,yy)
+        fig,ax = plt.subplots(1,2,figsize=(12,6),sharey=True)
+        
+        pc = ax[0].pcolor(xx,yy,count,shading='flat',cmap='Greys')
+        ax[0].set_aspect(1.0)
+        fig.colorbar(pc,ax=ax[0])
+        Lx = config['Lx']
+        Ly = config['Ly']/2
+        ax[0].add_patch(Polygon([[0,-Ly],[0,Ly],[Lx,Ly],[Lx,-Ly]],\
+                facecolor='none', edgecolor='r'))
+                
+        ax[1].plot(x,y,'k,')
+        ax[1].add_patch(Polygon([[0,-Ly],[0,Ly],[Lx,Ly],[Lx,-Ly]],\
+                facecolor='none', edgecolor='r'))
+        ax[1].set_aspect(1.0)
+        
+        
+        info = f'File: {infile}\nNx:{config["Nx"]}  Ny:{config["Ny"]}  Lx:{config["Lx"]}  Ly:{config["Ly"]}  dshift:{config["dshift"]}'
+        fig.text(0.5,0.95,info,ha='center',va='center')
+        
+        if not target.endswith('.png'):
+            target = target + '.png'
+        
+        fig.savefig(target)
+        
     elif args[0].lower() == 'view':
         infile = args[1]
         
